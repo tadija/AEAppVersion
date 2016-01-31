4377b4c6cf5e2040979e9012d5f843d1f7301697
Marko Tadic <tadija@me.com>
2016-01-31 19:28:12 +0100

    Initial Commit

---
+++ AEAppVersionManager/AEAppVersionManager.h
@@ -0,0 +1,19 @@
+//
+//  AEAppVersionManager.h
+//  AEAppVersionManager
+//
+//  Created by Marko Tadic on 1/31/16.
+//  Copyright © 2016 AE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AEAppVersionManager.
+FOUNDATION_EXPORT double AEAppVersionManagerVersionNumber;
+
+//! Project version string for AEAppVersionManager.
+FOUNDATION_EXPORT const unsigned char AEAppVersionManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AEAppVersionManager/PublicHeader.h>
+
+
---
+++ AEAppVersionManagerTests/AEAppVersionManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  AEAppVersionManagerTests.swift
+//  AEAppVersionManagerTests
+//
+//  Created by Marko Tadic on 1/31/16.
+//  Copyright © 2016 AE. All rights reserved.
+//
+
+import XCTest
+@testable import AEAppVersionManager
+
+class AEAppVersionManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
